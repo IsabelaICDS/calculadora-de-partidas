1351718fb994f5305cad3d4fc1f0f1b96ddb4680
isabela <isabelacsouza.arq@gmail.com>
2024-02-19 13:43:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function calculoNivel(vitorias, derrotas) {
+    var saldoVitorias = vitorias - derrotas;
+    var nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return {
+        saldoVitorias: saldoVitorias,
+        nivel: nivel
+    };
+}
+
+s
+var vitorias = 50;
+var derrotas = 20;
+var resultado = calculoNivel(vitorias, derrotas);
+console.log("O Herói tem de saldo de " + resultado.saldoVitorias + " está no nível de " + resultado.nivel);
